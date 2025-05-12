77ada462f25c3e163b771a66da2d7db796c7c741
DESKTOP-09O9IF1\soldesk <aja1533@gmail.com>
2025-05-12 17:19:13 +0900

    second

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,10 @@ import frame.MainFrame;
 public class Main {
     public static void main(String[] args) {
 
+    	private String name = "kim";
+    	
+    	System.out.println(name);
+    	
         new MainFrame();
     }
 }
